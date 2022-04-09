92a44e5cc6a138ba43c9a3347c81843b3fa7b0dd
Nimsilu <101490767+Nimsilu@users.noreply.github.com>
2022-04-09 14:37:33 +0530

    Update game_api.py

--- game_api.py
+++ game_api.py
@@ -26,8 +26,8 @@ class Game_api:
         game_queue_thread = Thread(target=self._watch_game_stream, daemon=True)
         game_queue_thread.start()
        
-        self.api.send_chat_message(self.game_id, "player", "Hello! You playing with {me} .")
-        self.api.send_chat_message(self.game_id, "player", "I wish you good luck {oppenent}!")
+        self.api.send_chat_message(self.game_id, "player", "Hello! You playing with BOT Dr-RespectBot.")
+        self.api.send_chat_message(self.game_id, "player", "I wish you good luck {opponent}!")
 
         self.api.send_chat_message(self.game_id, "spectator", "Welcome friends!")
         self.api.send_chat_message(self.game_id, "spectator", "Thanks for watching my games!")
