736761a4e1e2494747d113db60a1adf7e249a9b5
Nimsilu <101490767+Nimsilu@users.noreply.github.com>
2022-04-13 11:17:53 +0530

    Update startbot.sh

--- startbot.sh
+++ startbot.sh
@@ -5,6 +5,6 @@ chmod +x engines/stockfish_14.1_linux_x64_popcnt
 
 ### ONLY UNCOMMENT ONE START POINT ###
 
-#python heroku_challenges.py
+python heroku_challenges.py
          
-python heroku_matchmaking.py        # This start point will challenge other bots and decline all challenges. CAUTION!!! Don't run this 24/7, you will get rate limited by lichess.
+#python heroku_matchmaking.py        # This start point will challenge other bots and decline all challenges. CAUTION!!! Don't run this 24/7, you will get rate limited by lichess.
